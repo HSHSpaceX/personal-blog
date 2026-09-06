adbdbd2f9474d1b163df9e6ef0c95a14f54ad040
HSHSpaceX <HSH20130730@outlook.com>
2026-09-06 17:36:10 +0800

    创建图册：无

--- js/albums.js
+++ js/albums.js
@@ -1,3 +1,10 @@
 /* 图册数据：在画廊页右上角“+”创建和管理。 */
 window.SITE_ALBUMS = [
+  {
+    "id": "almtpm9x5imfic",
+    "title": "无",
+    "visibility": "public",
+    "photos": [],
+    "created": "2026-09-06"
+  }
 ];
