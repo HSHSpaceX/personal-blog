ec8861571b99f59ebec562820d4484a7b01d6e4f
HSHSpaceX <HSH20130730@outlook.com>
2026-09-06 17:54:21 +0800

    更新图册照片：无

--- js/albums.js
+++ js/albums.js
@@ -4,7 +4,12 @@ window.SITE_ALBUMS = [
     "id": "almtpm9x5imfic",
     "title": "无",
     "visibility": "public",
-    "photos": [],
+    "photos": [
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtpmxacwzvio.jpeg",
+        "caption": ""
+      }
+    ],
     "created": "2026-09-06"
   },
   {
