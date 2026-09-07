671d87921739bd20f05df64bcb63373ad8c82b7b
HSHSpaceX <HSH20130730@outlook.com>
2026-09-07 13:05:10 +0800

    更新图册照片：无

--- js/albums.js
+++ js/albums.js
@@ -4,7 +4,64 @@ window.SITE_ALBUMS = [
     "id": "almtpm9x5imfic",
     "title": "无",
     "visibility": "public",
-    "photos": [],
+    "photos": [
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0gutqogc.png",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0iznn1am.png",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0kmm1wnv.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0m0ed6p2.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0oiqcnpa.png",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0qkm27lb.png",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0t6lprv2.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0v74ji5n.png",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0x08lsg7.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs0yvbju09.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs114tq73a.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs14bkvjyi.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs16q3rbtb.jpeg",
+        "caption": ""
+      },
+      {
+        "src": "https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/gallery/almtpm9x5imfic/almtqs19dlpyyn.jpeg",
+        "caption": ""
+      }
+    ],
     "created": "2026-09-06"
   },
   {
