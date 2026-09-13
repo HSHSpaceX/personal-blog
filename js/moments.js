9ae5fa4a4c1a073a07b1aa42fb963e005c68fcaf
HSHSpaceX <HSH20130730@outlook.com>
2026-09-13 18:54:24 +0800

    删除动态

--- js/moments.js
+++ js/moments.js
@@ -1,9 +1,2 @@
 /* 动态数据:在动态页登录后发布和管理。 */
-window.BLOG_MOMENTS = [
-  {
-    "id": "m20260913-185113-vkz0",
-    "time": "2026-09-13 18:51",
-    "text": "测试",
-    "image": "assets/moments/img-20260913-185111-pliq.jpg"
-  }
-];
+window.BLOG_MOMENTS = [];
