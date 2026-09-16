0fa07d7bba3570fcea2a08290d1352900bf43ac4
HSHSpaceX <HSH20130730@outlook.com>
2026-09-16 20:26:16 +0800

    创建图册：朱雀2E

--- js/albums.js
+++ js/albums.js
@@ -1,2 +1,10 @@
 /* 图册数据：在画廊页右上角“+”创建和管理。 */
-window.SITE_ALBUMS = [];
+window.SITE_ALBUMS = [
+  {
+    "id": "almu42r9g0alb6",
+    "title": "朱雀2E",
+    "visibility": "public",
+    "photos": [],
+    "created": "2026-09-16"
+  }
+];
