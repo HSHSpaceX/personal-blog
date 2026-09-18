60756e882384b0248eeb002f109a189ef8a06237
HSHSpaceX <HSH20130730@outlook.com>
2026-09-18 19:45:45 +0800

    删除文章：测试篇

--- js/posts.js
+++ js/posts.js
@@ -1,15 +1,3 @@
 /* 文章数据：修改或新增文章后，首页、归档和文章页会自动更新。 */
 window.BLOG_POSTS = [
-  {
-    slug: "post-20260916-2019",
-    title: "测试篇",
-    category: "随笔",
-    tags: ["测试"],
-    date: "2026-09-18",
-    readingTime: 1,
-    cover: "assets/covers/cover-20260916-2019-b2ob.jpg",
-    featured: true,
-    excerpt: "",
-    content: `<p></p><div>图片测试</div><div><img src="https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/posts/img-20260918-1301-82oo.png" alt="插图"></div><div><br></div><div><img src="https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/posts/img-20260916-2021-b0eo.jpg" alt="插图">公式测试</div><div>$$t=s/t$$</div><div><br></div><div>目录测试</div><h2>测试</h2><div><br></div><div>链接测试</div><div><a href="https://www.kechuang.org/">https://www.kechuang.org/</a><br></div><div><br></div><div>视频测试</div><div><img src="https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/posts/img-20260917-2144-86dy.gif" alt="插图"></div><div><figure><video controls="" playsinline="" webkit-playsinline="" preload="metadata" src="https://raw.githubusercontent.com/HSHSpaceX/personal-blog/main/assets/videos/video-20260918-1308-g71f.mov" style="max-width:100%"></video></figure><br></div>`
-  }
 ];
