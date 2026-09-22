07792a696519160b223995af54e57e35b7d15272
HSHSpaceX <HSH20130730@outlook.com>
2026-09-22 21:19:07 +0800

    更新评论

--- js/comments.js
+++ js/comments.js
@@ -63,14 +63,6 @@ window.SITE_COMMENTS = {
       "email": "",
       "time": "2026-09-22",
       "content": "3"
-    },
-    {
-      "id": "c1790075298064",
-      "slug": "moment-m20260919-201800-zbue",
-      "nick": "HSH",
-      "email": "",
-      "time": "2026-09-22",
-      "content": "4"
     }
   ]
 };
