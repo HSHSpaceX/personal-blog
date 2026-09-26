490d3d627165770f20bb33d993f284d70625d99c
HSHSpaceX <HSH20130730@outlook.com>
2026-09-26 17:27:23 +0800

    新增评论：HSH(站长)

--- js/comments.js
+++ js/comments.js
@@ -1,7 +1,17 @@
 /* 评论数据：在后台“消息”栏目中管理。 */
 window.SITE_COMMENTS = {
   "post-20260906-1212": [],
-  "about": [],
+  "about": [
+    {
+      "id": "c1790414842733",
+      "slug": "about",
+      "nick": "HSH(站长)",
+      "email": "",
+      "time": "2026-09-26",
+      "content": "测试",
+      "device": "Mac"
+    }
+  ],
   "moment-m20260914-204633-fp3i": [],
   "post-20260918-2110": [],
   "moment-m20260919-201800-zbue": []
