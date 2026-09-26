8a342e177f2eb01190af115b54684c9521ff128d
HSHSpaceX <HSH20130730@outlook.com>
2026-09-26 17:05:45 +0800

    更新评论

--- js/comments.js
+++ js/comments.js
@@ -9,16 +9,6 @@ window.SITE_COMMENTS = {
       "email": "",
       "time": "2026-09-06",
       "content": "大家可以在这里发表评论，我会回复"
-    },
-    {
-      "id": "c1789822115197",
-      "slug": "about",
-      "nick": "HSH",
-      "email": "",
-      "time": "2026-09-19",
-      "content": "一万六😄",
-      "parentId": "c1789820778853",
-      "parentNick": "moya"
     }
   ],
   "moment-m20260914-204633-fp3i": [],
